f0897c014f96ddfc7cbeb7dfc6bbe242ee55bf84
Sam <sam.wheat@outlook.com>
2023-04-20 11:21:19 -0700

    fix pecent of row columns

--- LeaderPivot/MeasureData.cs
+++ LeaderPivot/MeasureData.cs
@@ -5,15 +5,11 @@ public class MeasureData<T> : IMeasureData<T>
     public IEnumerable<T> Measure { get; set; }
     public IEnumerable<T> RowGroup { get; set; }
     public IEnumerable<T> ColumnGroup { get; set; }
-    public IDimensionT<T> RowDimension { get; set; }
-    public IDimensionT<T> ColumnDimension { get; set; }
 
-    public MeasureData(IEnumerable<T> measure, IEnumerable<T> rowGroup, IEnumerable<T> columnGroup, IDimensionT<T> rowDimension, IDimensionT<T> columnDimension)
+    public MeasureData(IEnumerable<T> measure, IEnumerable<T> rowGroup, IEnumerable<T> columnGroup)
     {
         Measure = measure;
         RowGroup = rowGroup;
         ColumnGroup = columnGroup;
-        RowDimension = rowDimension;
-        ColumnDimension = columnDimension;
     }
 }
