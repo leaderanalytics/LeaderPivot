28881dc36b97cc7f1d7ca17641d34848215048b5
Sam <sam.wheat@outlook.com>
2023-12-17 11:55:34 -0800

    Add tests

---
+++ LeaderPivot.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace LeaderPivot.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
